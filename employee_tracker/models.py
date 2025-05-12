8f5d70fb8d2516249e47cb4ece538dac986a2ad0
MrYonatan <yonatanassefa60@gmail.com>
2025-05-12 21:39:08 +0300

    validation for duplicate items

--- employee_tracker/models.py
+++ employee_tracker/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 from django.forms import ValidationError
+from django.core.exceptions import ValidationError as DjangoValidationError
+
+class DuplicateEntryError(DjangoValidationError):
+    """Custom error for duplicate entries"""
+    def __init__(self, model_name, field_name, value):
+        message = f"A {model_name} with the {field_name} '{value}' already exists. Please use a different {field_name}."
+        super().__init__(message)
+
+def validate_unique_case_insensitive(value, model_class, field_name, instance=None):
+    """Validate that the value is unique case-insensitively"""
+    lookup = {f"{field_name}__iexact": value}
+    if instance and instance.pk:
+        lookup['pk__ne'] = instance.pk
+    
+    if model_class.objects.filter(**lookup).exists():
+        model_name = model_class.__name__.lower()
+        raise DuplicateEntryError(model_name, field_name, value)
 
 class Division(models.Model):
     class DivisionType(models.TextChoices):
@@ -17,13 +34,31 @@ class Division(models.Model):
         return self.name
 
 class SectorSubdivisionType(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+
+    def clean(self):
+        super().clean()
+        validate_unique_case_insensitive(self.name, SectorSubdivisionType, 'name', self)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 class Subcity(models.Model):
     name = models.CharField(max_length=50)
+
+    def clean(self):
+        super().clean()
+        validate_unique_case_insensitive(self.name, Subcity, 'name', self)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -43,11 +78,35 @@ class SubcitySubdivisionType(models.Model):
 
 class Woreda(models.Model):
     name = models.CharField(max_length=50)
-    subcity = models.ForeignKey(Subcity, on_delete=models.PROTECT)      # cant delete subcity if linked to woreda
+    subcity = models.ForeignKey(Subcity, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = ['name', 'subcity']
+
+    def clean(self):
+        super().clean()
+        # Check for case-insensitive uniqueness within the same subcity
+        lookup = {
+            'name__iexact': self.name,
+            'subcity': self.subcity
+        }
+        if self.pk:
+            lookup['pk__ne'] = self.pk
+        
+        if Woreda.objects.filter(**lookup).exists():
+            raise DuplicateEntryError(
+                'woreda',
+                'name',
+                f"{self.name} in {self.subcity.name}"
+            )
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} - {self.subcity}"
-
 
 class OrganizationalUnit(models.Model):
     name = models.CharField(max_length=255)
@@ -68,9 +127,10 @@ class OrganizationalUnit(models.Model):
     def clean(self):
         super().clean()
         if self.sector_subdiv_type is not None and self.subcity_subdiv_type is not None:
-            raise ValidationError(
+            raise DjangoValidationError(
                 "Sector Subdivision Type and Subcity Subdivision Type are mutually exclusive. You cannot select both."
             )
+        validate_unique_case_insensitive(self.name, OrganizationalUnit, 'name', self)
     
     def save(self, *args, **kwargs):
         # If this unit has a parent, inherit the fields if they're not set
@@ -86,6 +146,8 @@ class OrganizationalUnit(models.Model):
             if not self.woreda:
                 self.woreda = self.parent.woreda
         
+        self.name = self.name.upper()
+        self.full_clean()
         super().save(*args, **kwargs)
                 
     def __str__(self):
@@ -110,7 +172,7 @@ class Employee(models.Model):
     fname = models.CharField(max_length=255)
     mname = models.CharField(max_length=255)
     lname = models.CharField(max_length=255)
-    phone_no = models.CharField(max_length=50)
+    phone_no = models.CharField(max_length=50, unique=True)
     organizationalunit = models.ForeignKey(OrganizationalUnit, on_delete=models.PROTECT)
     role = models.ForeignKey(EmployeeRole, on_delete=models.PROTECT, null=True, blank=True)
 
