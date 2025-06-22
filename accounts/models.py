318dfe60bb6f3a998104c2c538f81908d33c841a
MrYonatan <yonatanassefa60@gmail.com>
2025-06-22 12:54:56 +0300

    cleaned up

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    pass
--- employee_tracker/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployeeTrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'employee_tracker'
-    
-    def ready(self):
-        import employee_tracker.signals
